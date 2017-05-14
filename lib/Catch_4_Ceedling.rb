fd55b34dec6e63e5695033839cc909a531263391
Patrick Junger <mail@pjunger.com>
2017-05-14 17:17:26 +0200

    Removed empty yml file, removed cpp extensions and added some workarounds (and monkeypatching stuff)

--- lib/Catch_4_Ceedling.rb
+++ lib/Catch_4_Ceedling.rb
@@ -1,7 +1,7 @@
 require 'ceedling/plugin'
 require 'catch_testrunner_generator'
 
-class Catch < Plugin
+class Catch4_Ceedling < Plugin
 
   # Set up Ceedling to use this plugin.
   def setup
@@ -9,11 +9,14 @@ class Catch < Plugin
     @plugin_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
     puts "Using Catch..."
 
+    GeneratorTestRunner.set_main_location(@plugin_root)
     # Switch out the The unity test runner with the catchy thing.
-    @ceedling[:generator_test_runner] = CatchTestRunnerGenerator.new(@ceedling[:setupinator].config_hash[:unity])
+    # @ceedling[:generator_test_runner] = CatchTestRunnerGenerator.new()
+    # @ceedling[:generator_test_runner].find_tests = CatchTestRunnerGenerator.find_tests
+    # @ceedling[:generator_test_runner].generate = CatchTestRunnerGenerator.generate
 
     # Add the path to catch.hpp to the include paths.
-    COLLECTION_PATHS_TEST_SUPPORT_SOURCE_INCLUDE_VENDOR << "#{@plugin_root}/vendor/catch/"
+    COLLECTION_PATHS_TEST_SUPPORT_SOURCE_INCLUDE_VENDOR << "#{@plugin_root}/vendor/Catch/single_include"
     # Add the interfaces to includes
     COLLECTION_PATHS_TEST_SUPPORT_SOURCE_INCLUDE_VENDOR << "#{@plugin_root}/src/"
   end
--- src/catch_main.c
+++ src/catch_main.c
@@ -1,8 +1,8 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-#include "mock_interface.hpp"
-#include "reporter_interface.hpp"
+#include "mock_interface.h"
+#include "reporter_interface.h"
 
 struct MockListener : Catch::TestEventListenerBase {
 
