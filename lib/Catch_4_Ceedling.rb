770833711b4628ec3e7e39f100352d032d0a8cc5
Patrick Junger <patrick.junger@gmail.com>
2017-05-21 21:17:43 +0200

    #1

--- lib/Catch_4_Ceedling.rb
+++ lib/Catch_4_Ceedling.rb
@@ -9,6 +9,8 @@ class Catch4_Ceedling < Plugin
     # Get the location of this plugin.
     @plugin_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
     puts "Using Catch..."
+    @main_c = ''
+    @main_o = ''
 
     GeneratorTestRunner.set_main_location(@plugin_root)
     # Switch out the The unity test runner with the catchy thing.
@@ -19,6 +21,26 @@ class Catch4_Ceedling < Plugin
     COLLECTION_PATHS_TEST_SUPPORT_SOURCE_INCLUDE_VENDOR << "#{@plugin_root}/vendor/Catch/single_include"
     # Add the interfaces to includes
     COLLECTION_PATHS_TEST_SUPPORT_SOURCE_INCLUDE_VENDOR << "#{@plugin_root}/src/"
+  end
+
+  def compile_main_file_if_nonexisting()
+    # Compile only once, it should be invariant
+  end
+
+  #  { :tool => tool,
+  #    :context => context,
+  #    :objects => objects,
+  #    :executable => executable,
+  #    :map => map,
+  #    :libraries => libraries
+  #  }
+  def pre_link_execute(arg_hash)
+    # if gcc, change to g++?
+    # or if gcc, add stl?
+
+    # if arg_hash[:tool][:]
+    
+    # Add compiled main_file to :objects
   end
 
 end
