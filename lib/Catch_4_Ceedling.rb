b69e7810fb15cb40f3727ef2d254b305a95a6534
Patrick Junger <pJunger@users.noreply.github.com>
2017-09-23 23:46:10 +0200

    Added asynchronous compilation of catch_main

--- lib/Catch_4_Ceedling.rb
+++ lib/Catch_4_Ceedling.rb
@@ -4,6 +4,13 @@ require 'catch_reporter'
 
 require 'fileutils'
 
+begin 
+  require 'concurrent'
+rescue LoadError => e
+  puts 'No Concurrent Ruby found: Please execute "gem install concurrent-ruby"' if e.message =~ /concurrent/
+  raise
+end
+
 class Catch4_Ceedling < Plugin
   # Get the location of this plugin.
   
@@ -15,29 +22,45 @@ class Catch4_Ceedling < Plugin
     @out_file = File.join(@out, 'catch_main.o')
 
     @catch_file = File.join(@plugin_root, 'src', 'catch_main.cpp')
-
     # Add the path to catch.hpp to the include paths.
     COLLECTION_PATHS_TEST_SUPPORT_SOURCE_INCLUDE_VENDOR << "#{@plugin_root}/vendor/Catch/single_include"
     # Add the interfaces to includes
     COLLECTION_PATHS_TEST_SUPPORT_SOURCE_INCLUDE_VENDOR << "#{@plugin_root}/src/"
+    @compile_future = nil
   end
 
   def pre_runner_generate(arg_hash)
     GeneratorTestRunner.set_context(@ceedling, @plugin_root)
-    compile_main()
+  end
+  
+  def pre_compile_execute(arg_hash)
+    if @compile_future.nil?
+      @compile_future = Concurrent::Future.execute {
+        puts 'Starting compiling catch_main.c' 
+        compile_main(arg_hash) 
+        puts 'Finished compiling catch_main.c'
+      }
+    end
   end
 
-  def compile_main()
+  def pre_link_execute(arg_hash)
+    # The object file is needed before linking, so we will wait here
+    if @compile_future.incomplete?
+      @compile_future.wait_or_cancel(20)
+    end
+  end
+
+  def compile_main(arg_hash)
     # Todo: Replace through proper rake call respectively add as dependency to ceedling (and then remove linker argument from .yml)
+    
     # Optimization: Create extra catch_main file to reduce compilation times
     # Compile only once, it should be invariant
-
-    unless File.file?(@out_file)
+    if ((arg_hash[:context] != RELEASE_SYM) and (not File.file?(@out_file)))
       FileUtils::mkdir_p(@main_dir)
       @ceedling[:generator].generate_object_file(
-        TOOLS_TEST_COMPILER,
-        OPERATION_COMPILE_SYM,
-        TEST_SYM,
+        arg_hash[:tool],
+        arg_hash[:operation],
+        arg_hash[:context],
         @catch_file,
         @out_file,
         @ceedling[:file_path_utils].form_test_build_list_filepath( @out_file ) 
